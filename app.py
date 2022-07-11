9efc775cb22ed168ffcdd46361921a24f3c542c5
Zeb Khan <zeb.khan@hotmail.co.uk>
2022-07-11 10:15:59 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from views import my_view
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+app.register_blueprint(my_view)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", e=e)
+
+if __name__=="__main__":
+    app.run(debug=True, port=8000) 
---
+++ static/app.js
@@ -0,0 +1,163 @@
+function darkCheck() {
+    let element = document.body;
+
+    let state = localStorage.getItem("state");
+    if (state =="dark"){
+        element.classList.toggle("lightswitch");
+    } 
+
+
+
+(function () {
+    let lightSwitch = document.getElementById('lightSwitch');
+    if (!lightSwitch) {
+    return;
+    }
+
+//
+//@function darkmode
+//@summary: changes the theme to 'dark mode' and save settings to local stroage.
+//Basically, replaces/toggles every CSS class that has '-light' class with '-dark'
+    
+function darkMode() {
+    document.querySelectorAll('.bg-light').forEach((element) => {
+        element.className = element.className.replace(/-light/g, '-dark');
+    });
+
+document.body.classList.add('bg-dark');
+
+if (document.body.classList.contains('text-dark')) {
+        document.body.classList.replace('text-dark', 'text-light');
+    } else {
+        document.body.classList.add('text-light');
+    }
+
+// Tables
+    var tables = document.querySelectorAll('table');
+for (var i = 0; i < tables.length; i++) {
+        // add table-dark class to each table
+        tables[i].classList.add('table-dark');
+    }
+
+      // set light switch input to true
+if (!lightSwitch.checked) {
+        lightSwitch.checked = true;
+    }
+    localStorage.setItem('lightSwitch', 'dark');
+    }
+
+    //
+    //@function lightmode
+    //@summary: changes the theme to 'light mode' and save settings to local stroage.
+    
+function lightMode() {
+    document.querySelectorAll('.bg-dark').forEach((element) => {
+        element.className = element.className.replace(/-dark/g, '-light');
+    });
+
+document.body.classList.add('bg-light');
+
+if (document.body.classList.contains('text-light')) {
+        document.body.classList.replace('text-light', 'text-dark');
+    } else {
+        document.body.classList.add('text-dark');
+    }
+
+      // Tables
+    var tables = document.querySelectorAll('table');
+for (var i = 0; i < tables.length; i++) {
+        if (tables[i].classList.contains('table-dark')) {
+        tables[i].classList.remove('table-dark');
+        }
+    }
+
+if (lightSwitch.checked) {
+        lightSwitch.checked = false;
+    }
+    localStorage.setItem('lightSwitch', 'light');
+    }
+
+    //
+    //@function onToggleMode
+    //@summary: the event handler attached to the switch. calling @darkMode or @lightMode depending on the checked state.
+    
+function onToggleMode() {
+    if (lightSwitch.checked) {
+        darkMode();
+    } else {
+        lightMode();
+    }
+    }
+
+
+    //@function getSystemDefaultTheme
+    //@summary: get system default theme by media query
+    
+function getSystemDefaultTheme() {
+    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
+    if (darkThemeMq.matches) {
+        return 'dark';
+    }
+    return 'light';
+    }
+
+    function setup() {
+var settings = localStorage.getItem('lightSwitch');
+    if (settings == null) {
+        settings = getSystemDefaultTheme();
+    }
+
+if (settings == 'dark') {
+        lightSwitch.checked = true;
+    }
+
+lightSwitch.addEventListener('change', onToggleMode);
+    onToggleMode();
+    }
+
+    setup();
+})();
+}
+
+
+// //Get the button
+// let mybutton = document.getElementById("btn-back-to-top");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+// scrollFunction();
+// };
+
+// function scrollFunction() {
+// if (
+// document.body.scrollTop > 20 ||
+// document.documentElement.scrollTop > 20
+// ) {
+// mybutton.style.display = "block";
+// } else {
+// mybutton.style.display = "none";
+// }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// //mybutton.addEventListener("click", backToTop);
+
+// function backToTop() {
+// document.body.scrollTop = 0;
+// document.documentElement.scrollTop = 0;
+// }
+$(document).ready(function() {
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+        $('#toTopBtn').fadeIn();
+    } else {
+        $('#toTopBtn').fadeOut();
+    }
+    });
+
+    $('#toTopBtn').click(function() {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 1000);
+    return false;
+    });
+});
